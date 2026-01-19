3b70c77c3ee74ed058a9f6136e5071f053ed0c58
andresjimenez6 <andrescortijo2006@gmail.com>
2026-01-19 11:47:36 +0100

    Inicio de la clase Productos

---
+++ TiendaOnline_ej3/src/tiendaonline_ej3/Productos.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiendaonline_ej3;
+
+
+public class Productos {
+    // DeclaraciÃ³n de atributos
+    
+private String nombre;
+private int precio;
+
+// Constructor por defecto
+
+public Productos(){
+    this.nombre = "";
+    this.precio = 0;
+}
+
+// Constructor por parÃ¡metros
+    
+public Productos(String nombre, int precio){
+    this.nombre = nombre;
+    this.precio = precio;
+}
+
+// Constructor de copia
+    
+public Productos(Productos productosACopiar){
+    this.nombre = productosACopiar.nombre;
+    this.precio = productosACopiar.precio;
+}
+
+// DeclaraciÃ³n de getters (1 por atributo)
+    
+public  String getNombre(){
+    return this.nombre;
+}
+public int getPrecio(){
+    return this.precio;
+}
+
+// DeclaraciÃ³n de setters (1 por atributo)
+    public void setNombre(String nombre){
+    this.nombre = nombre;
+}
+public void setPrecio(int precio){
+     this.precio = precio;
+}
+
+
+// DeclaraciÃ³n de mÃ©todos personalizados
+    
+// DeclaraciÃ³n toString() (con @Override)
+
+
+}
+
