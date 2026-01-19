16f2c292014dc25b6df5df4b9a1b358f7261e500
andresjimenez6 <andrescortijo2006@gmail.com>
2026-01-19 14:25:41 +0100

    avances en la clase pedidos

--- TiendaOnline_ej3/src/tiendaonline_ej3/Pedido.java
+++ TiendaOnline_ej3/src/tiendaonline_ej3/Pedido.java
@@ -5,10 +5,56 @@
  */
 package tiendaonline_ej3;
 
-/**
- *
- * @author EAG
- */
+import tiendaonline_ej3.Productos;
+
 public class Pedido {
     
+// Declaración de atributos
+private int importe;
+private Productos[] productos;
+// Constructor por defecto
+public Pedido(){
+    this.importe = 0;
+    this.productos = new Productos[0];
 }
+// Constructor por parámetros
+public Pedido(int importe, Productos[] productos){
+    this.importe = importe;
+    this.productos = productos.clone();
+}
+// Constructor de copia
+public Pedido(Pedido pato){
+    this.importe = pato.importe;
+    this.productos = pato.productos.clone();
+}
+// Declaración de getters (1 por atributo)
+public int getImporte(){
+ return this.importe;
+}
+public Productos[] getProductos(){
+    return this.productos;
+}
+// Declaración de setters (1 por atributo)
+public void setProductos(Productos[] productos){
+    this.productos = productos;
+}
+public void setImporte( int importe){
+    this.importe = importe;
+}
+// Declaración de métodos personalizados
+public boolean añadirProducto(){
+    
+    
+    Productos[] carrito = new Productos[this.productos.length+1];
+    
+    
+    for (int i = 0; i < carrito.length -1; i++) {
+        carrito[i] = this.productos[i];
+    }
+
+
+}
+// Declaración toString() (con @Override)
+    
+    
+
